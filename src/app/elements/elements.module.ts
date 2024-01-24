6ddfe631e5d285f2391fcd7c625e30b4f696f40f
Aman07a <aman16.aa17@gmail.com>
2024-01-24 15:39:16 +0100

    Implementing a Titled Divider

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceholderComponent],
-  imports: [CommonModule, ElementsRoutingModule],
+  imports: [CommonModule, ElementsRoutingModule, SharedModule],
   exports: [],
 })
 export class ElementsModule {}
