8e85a83ca6a5a70057852e63036a477b41aa6f59
Aman07a <aman16.aa17@gmail.com>
2024-01-24 16:31:05 +0100

    Communicating Down Table Data

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
-export class TableComponent {}
+export class TableComponent implements OnInit {
+  @Input() data = [] as any;
+  @Input() headers = [] as any;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
