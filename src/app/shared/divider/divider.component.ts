918fa7f2845776fe44a98dc5ad7588cbbad280ca
Aman07a <aman16.aa17@gmail.com>
2024-01-24 15:46:31 +0100

    Grabbing Content with NgContent

--- src/app/shared/divider/divider.component.ts
+++ src/app/shared/divider/divider.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-divider',
   templateUrl: './divider.component.html',
   styleUrls: ['./divider.component.css'],
 })
-export class DividerComponent {}
+export class DividerComponent implements OnInit {
+  @Input() title = '';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
