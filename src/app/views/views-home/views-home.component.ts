f10920e839c6967f29feff3cadadc9d0e4acd00a
Aman07a <aman16.aa17@gmail.com>
2024-01-25 09:30:08 +0100

    Displaying a List of Statistics

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-views-home',
   templateUrl: './views-home.component.html',
   styleUrls: ['./views-home.component.css'],
 })
-export class ViewsHomeComponent {}
+export class ViewsHomeComponent implements OnInit {
+  stats = [
+    { value: 22, label: '# of Users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Reviews' },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
