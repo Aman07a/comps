ee87059d0167fff4872757073697021b53a7ed03
Aman07a <aman16.aa17@gmail.com>
2024-01-25 09:44:09 +0100

    Displaying a List of Items

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
-import { SharedModule } from '../shared/shared.module';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
-  declarations: [ViewsHomeComponent, StatisticsComponent],
+  declarations: [ViewsHomeComponent, StatisticsComponent, ItemListComponent],
   imports: [CommonModule, ViewsRoutingModule, SharedModule],
 })
 export class ViewsModule {}
